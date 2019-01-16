6a03d38bb7fd3b1c995b47d640784c6ef3e9dc4c
Admin <admin@Admins-Mac.local>
2019-01-16 23:14:44 +0300

    Initial Commit

---
+++ PriceCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PriceCalculator
+//
+//  Created by Admin on 16.01.2019.
+//  Copyright © 2019 furrki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
