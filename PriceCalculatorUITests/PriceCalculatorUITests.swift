3038bc5dec3e85273e3e0a2e6569cd742f941b23
Admin <admin@Admins-Mac.local>
2019-01-17 19:52:52 +0300

    Design Bugfix after Tests

--- PriceCalculatorUITests/PriceCalculatorUITests.swift
+++ PriceCalculatorUITests/PriceCalculatorUITests.swift
@@ -47,9 +47,5 @@ class PriceCalculatorUITests: XCTestCase {
         XCTAssertEqual(app.staticTexts["hoursLabel"].isHittable, true)
         
     }
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
 }
